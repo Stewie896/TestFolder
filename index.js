e345dc7fe84cf1dad6b7cdde033defaf422f98f1
abhiral chhetri <abhiralchhetri6@gmail.com>
2025-05-06 13:18:02 +0530

    Overload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const hello = "HELLO WORLD";
+const keno = "HELLO WORLD";
 let num = 12;
 
 
@@ -19,3 +19,21 @@ console.log(hello())
 
 console.log(func(10 , 10))
 console.log(func2(10 , 10))
+
+
+class Hsl{
+    constructor(name , age , password) {
+       this.name = name;
+       this.age = age;
+       this.password = password ;
+       this.yalo = "Yalo"
+    }
+
+    voidSayhello(){
+        console.log(`Iam saying your name is ${this.name} , your age is ${this.age } , and your passowrd is ${this.password}`)
+    }
+}
+
+const byby = new Hsl("abh"  , 17 , "asdfjko" );
+
+console.log(byby.yalo)
